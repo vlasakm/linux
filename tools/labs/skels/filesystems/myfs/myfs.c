f0b8f7188d5d3cc12a6451b4920543e2fbf2ccfb
Michal Vlasák <lahcim8@gmail.com>
2022-12-09 00:41:16 +0100

    Finish Lab 9 & 10 File System Dreivers

--- tools/labs/skels/filesystems/myfs/myfs.c
+++ tools/labs/skels/filesystems/myfs/myfs.c
@@ -27,21 +27,44 @@ static int myfs_create(struct inode *dir, struct dentry *dentry,
 static int myfs_mkdir(struct inode *dir, struct dentry *dentry, umode_t mode);
 
 /* TODO 2: define super_operations structure */
+static const struct super_operations myfs_super_operations = {
+	/* NOTE: ramfs uses dummy generic_delete_inode, which is not enough here
+	 */
+	.drop_inode = generic_drop_inode,
+	.statfs = simple_statfs,
+};
 
 static const struct inode_operations myfs_dir_inode_operations = {
 	/* TODO 5: Fill dir inode operations structure. */
+	.create = myfs_create,
+	.lookup = simple_lookup,
+	.link = simple_link,
+	.mkdir = myfs_mkdir,
+	.rmdir = simple_rmdir,
+	.unlink = simple_unlink,
+	.mknod = myfs_mknod,
+	.rename = simple_rename,
 };
 
 static const struct file_operations myfs_file_operations = {
 	/* TODO 6: Fill file operations structure. */
+         .llseek         = generic_file_llseek,
+         .read_iter      = generic_file_read_iter,
+         .write_iter     = generic_file_write_iter,
+         .mmap           = generic_file_mmap,
 };
 
 static const struct inode_operations myfs_file_inode_operations = {
 	/* TODO 6: Fill file inode operations structure. */
+	.setattr	= simple_setattr,
+	.getattr	= simple_getattr,
 };
 
 static const struct address_space_operations myfs_aops = {
 	/* TODO 6: Fill address space operations structure. */
+	.readpage	= simple_readpage,
+	.write_begin	= simple_write_begin,
+	.write_end	= simple_write_end,
 };
 
 struct inode *myfs_get_inode(struct super_block *sb, const struct inode *dir,
@@ -59,31 +82,71 @@ struct inode *myfs_get_inode(struct super_block *sb, const struct inode *dir,
 	 *     - atime,ctime,mtime
 	 *     - ino
 	 */
+	inode_init_owner(inode, dir, mode);
+	inode->i_atime = inode->i_mtime = inode->i_ctime = current_time(inode);
 
 	/* TODO 5: Init i_ino using get_next_ino */
+	inode->i_ino = get_next_ino();
 
 	/* TODO 6: Initialize address space operations. */
 
 	if (S_ISDIR(mode)) {
 		/* TODO 3: set inode operations for dir inodes. */
+		inode->i_op = &simple_dir_inode_operations;
+		inode->i_fop = &simple_dir_operations;
 
 		/* TODO 5: use myfs_dir_inode_operations for inode
 		 * operations (i_op).
 		 */
+		inode->i_op = &myfs_dir_inode_operations;
 
 		/* TODO 3: directory inodes start off with i_nlink == 2 (for "." entry).
 		 * Directory link count should be incremented (use inc_nlink).
 		 */
+		inc_nlink(inode);
 	}
 
 	/* TODO 6: Set file inode and file operations for regular files
 	 * (use the S_ISREG macro).
 	 */
+	if (S_ISREG(inode->i_mode)) {
+		inode->i_op = &myfs_file_inode_operations;
+		inode->i_fop = &myfs_file_operations;
+		inode->i_mapping->a_ops = &myfs_aops;
+	}
 
 	return inode;
 }
 
 /* TODO 5: Implement myfs_mknod, myfs_create, myfs_mkdir. */
+static int myfs_mknod(struct inode *dir, struct dentry *dentry, umode_t mode,
+		      dev_t dev)
+{
+	struct inode *inode = myfs_get_inode(dir->i_sb, dir, mode);
+
+	if (!inode)
+		return -EINVAL;
+
+	d_instantiate(dentry, inode);
+	dget(dentry);	/* Extra count - pin the dentry in core */
+	// ramfs has current_time(dir)
+	dir->i_mtime = dir->i_ctime = current_time(inode);
+	return 0;
+}
+static int myfs_create(struct inode *dir, struct dentry *dentry, umode_t mode,
+		       bool excl)
+{
+	return myfs_mknod(dir, dentry, mode | S_IFREG, 0);
+}
+static int myfs_mkdir(struct inode *dir, struct dentry *dentry, umode_t mode)
+{
+	int err = myfs_mknod(dir, dentry, mode | S_IFDIR, 0);
+	if (err != 0)
+		return err;
+
+	inc_nlink(dir);
+	return 0;
+}
 
 static int myfs_fill_super(struct super_block *sb, void *data, int silent)
 {
@@ -96,6 +159,11 @@ static int myfs_fill_super(struct super_block *sb, void *data, int silent)
 	 *   - super operations
 	 *   - maxbytes
 	 */
+	sb->s_blocksize = MYFS_BLOCKSIZE;
+	sb->s_blocksize_bits = MYFS_BLOCKSIZE_BITS;
+	sb->s_magic = MYFS_MAGIC;
+	sb->s_op = &myfs_super_operations;
+	sb->s_maxbytes = MAX_LFS_FILESIZE;
 
 	/* mode = directory & access rights (755) */
 	root_inode = myfs_get_inode(sb, NULL,
@@ -123,15 +191,23 @@ static struct dentry *myfs_mount(struct file_system_type *fs_type,
 		int flags, const char *dev_name, void *data)
 {
 	/* TODO 1: call superblock mount function */
+	return mount_nodev(fs_type, flags, data, myfs_fill_super);
 }
 
 /* TODO 1: define file_system_type structure */
+struct file_system_type myfs_fs_type = {
+	.owner = THIS_MODULE,
+	.name = "myfs",
+	.mount = myfs_mount,
+	.kill_sb = kill_litter_super,
+};
 
 static int __init myfs_init(void)
 {
 	int err;
 
 	/* TODO 1: register */
+	err = register_filesystem(&myfs_fs_type);
 	if (err) {
 		printk(LOG_LEVEL "register_filesystem failed\n");
 		return err;
@@ -143,6 +219,7 @@ static int __init myfs_init(void)
 static void __exit myfs_exit(void)
 {
 	/* TODO 1: unregister */
+	unregister_filesystem(&myfs_fs_type);
 }
 
 module_init(myfs_init);
